429643e4dade4f3158ac72f0b7eafe8fb9e3c43b
Hasan A <hsnaltinkaynak.us@gmail.com>
2020-10-17 00:04:36 -0400

    added cloudformation template for dev server

--- build-dev-docker-images.sh
+++ build-dev-docker-images.sh
@@ -7,6 +7,6 @@ docker build --force-rm -t "petclinic-customer-service:dev" ./spring-petclinic-c
 docker build --force-rm -t "petclinic-discover-server:dev" ./spring-petclinic-discover-server
 docker build --force-rm -t "petclinic-hystrix-dashboard:dev" ./spring-petclinic-hystrix-dashboard
 docker build --force-rm -t "petclinic-velt-service:dev" ./spring-petclinic-velt-service
-docker build --force-rm -t "petclinic-visits-service:dev" ./spring-petclinic-visits-service
+docker build --force-rm -t "petclinic-visits-service:dev"git pull  ./spring-petclinic-visits-service
 docker build --force-rm -t "petclinic-prometheus-server:dev" ./docker/prometheus
 docker build --force-rm -t "petclinic-grafana-server:dev" ./docker/grafana
